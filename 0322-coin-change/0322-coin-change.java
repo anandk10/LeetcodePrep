3ceafc5bd3d58e3aa47cf69a71f1c6a7a6f9c6fe
Anand Karandikar <anandk10@gmail.com>
2023-12-11 02:14:36 -0800

    Time: 160 ms (5.10%), Space: 45.3 MB (5.07%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,78 @@
+class Solution {
+
+    // top down
+    private int maxCoinsForTotal(int[] coins, int total, Map<Integer, Integer> map) {
+
+        if (total == 0) return 0;
+
+        if (map.containsKey(total)) return map.get(total);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int coin : coins) {
+            if (coin > total) {
+                continue;
+            } 
+
+            // calculate for total - coin
+            int val = maxCoinsForTotal(coins, total - coin, map);
+
+            // if min val of number of coins received then update
+            if (val < min) {
+                min = val;
+            }
+
+        }
+
+        // if min stays at Integer.MAX_VALUE that indicates that no possible 
+        // coin could sum upto the total
+        if (min != Integer.MAX_VALUE) {
+            // Adding 1 because we are choosing the current coin 
+            min = 1 + min;
+        }
+
+        // memoizing the value for total
+        map.put(total, min);
+
+        return min;
+    }
+
+    private int maxCoinsForTotalBottomUp(int[] coins, int total) {
+        int[] dp = new int[total + 1];
+
+        for (int j = 0 ; j <= total ; j++) {
+            dp[j] = Integer.MAX_VALUE;
+        }
+
+        dp[0] = 0;
+
+        for(int i = 0;  i < coins.length; i++) {
+            for (int j = 1; j <= total ; j++) {
+                if (j < coins[i]) continue;
+                int min = Math.min(dp[j], 1 + dp[j - coins[i]]);
+                dp[j] = min;
+            }
+        }
+
+        if (dp[total] == Integer.MAX_VALUE) return -1;
+        
+        return dp[total];
+
+    }
+
+    public int coinChange(int[] coins, int amount) {
+
+        if (amount <= 0) return 0;
+
+        System.out.println("Bottom up : " + maxCoinsForTotalBottomUp(coins, amount));
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int counts = maxCoinsForTotal(coins, amount, map);
+
+        if (counts == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return map.getOrDefault(amount, -1);
+    }
+}
