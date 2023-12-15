6c46b51a4a5224af50419f9a8f4df71c1605f10a
Anand Karandikar <anandk10@gmail.com>
2023-12-14 23:49:40 -0800

    Time: 0 ms (100.00%), Space: 42.9 MB (85.71%) - LeetHub

---
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.java
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    int diameter = 0;
+    
+    private int solution1(Node root) {
+        if (root == null) return 0;
+        
+        int max1 = 0;
+        int max2 = 0;
+        
+        for (Node child : root.children) {
+            int depth = solution1(child);
+            if (max1 < depth) {
+                max2 = max1;
+                max1 = depth;
+            } else if (max2 < depth) {
+                max2 = depth;
+            }
+        }
+        
+        diameter = Math.max(diameter, max1 + max2);
+        
+        return 1 + Math.max(max1, max2);
+        
+    }
+    
+    public int diameter(Node root) {
+        solution1(root);
+        return diameter;
+    }
+}
