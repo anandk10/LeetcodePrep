8f9786e4b4e0ff3f538dd48915acfedd835b19d1
Anand Karandikar <anandk10@gmail.com>
2023-12-17 23:08:58 -0800

    Time: 1 ms (85.24%), Space: 44.2 MB (64.60%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -42,7 +42,26 @@ class Solution {
         
     }
     
+    /**
+    A follow up on the valley and peak approach - 
+        where we look for the min and max points
+        however the smaller positive differences
+        could be just summed up to find the total 
+        profit
+    */
+    private int maxProfitWithSimpleOnePass(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            
+            if (prices[i - 1] < prices[i]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+    
     public int maxProfit(int[] prices) {
-        return maxProfitWithValleyAndPeak(prices);
+        // return maxProfitWithValleyAndPeak(prices);
+        return maxProfitWithSimpleOnePass(prices);
     }
 }
