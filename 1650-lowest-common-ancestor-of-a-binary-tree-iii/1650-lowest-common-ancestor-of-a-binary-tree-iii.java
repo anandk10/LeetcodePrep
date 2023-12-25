d70900f1b7ee30f2eff850d8b5d032f43d3c584c
Anand Karandikar <anandk10@gmail.com>
2023-12-24 20:58:52 -0800

    Time: 28 ms (72.37%), Space: 44.7 MB (35.10%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -8,99 +8,83 @@ class Node {
 };
 */
 
-/**
-This problem could be seen as finding the intersection of two lists at some node (LCA)
-Look at the TreeNode as LinkedList node, where parent is like the next pointer
-
-        4 ---> 3 ---> 2 ---> 1 ---> null
-6 ---> 5----> 10 ---/
-
-the root of the tree is 1, but parent pointer traversal start from any node in the tree would reach 
-its convergence at their LCA
-
-so if two nodes were 4 and 6, these lie in different subtrees divided by TreeNode 3
-
-We need three traversals
-from 4 to 1 (end of list) - count these
-from 6 to 1 (end of list) - count these
-
-4 (1-2-3-4) nodes vs 5 nodes ( 1-2-10-5-6 )
-the delta is 1
-so traverse the big  delta number of times
-
-and then start both the traversals at the same
-
-so final traversal will start at
-
-        4 ---> 3 ---> 2 ---> 1 ---> null
-        5----> 10 ---/
-
-if memory (4) == memory (5) then
-    lca found
-else
-    move both the lists
-
-if memory (3) == memory (10) then
-    lca found
-else
-    move both the lists
-
-if memory (2) == memory (2) then
-    lca found --- true
-else
-    move both the lists
-
-
- */
-
 class Solution {
 
-
-    private int getHeight(Node node) {
-        int count = 0;
-        while (node != null) {
-            node = node.parent;
-            count++;
-        }
-        return count;
-    }
-
-    private Node traverseDelta(Node node, int delta) {
-        while (node != null && delta > 0) {
-            node = node.parent;
-            delta--;
-        }
-        return node;
-    }
-
-
     public Node lowestCommonAncestor(Node p, Node q) {
+        
+        Node a = p;
+        Node b = q;
 
+        while (a != b) {
+            if (a == null) {
+                // start with the other list
+                a = q; 
+            } else {
+                a = a.parent;
+            }
 
-        // first count the nodes
-        int heightP = getHeight(p);
-        int heightQ = getHeight(q);
-
-        // System.out.println("Height p : " + heightP);
-
-        // System.out.println("Height q : " + heightQ);
-
-        Node higher = null;
-        Node lower = null;
-
-        if (heightP > heightQ) {
-            higher = traverseDelta(p, heightP - heightQ);
-            lower = q;
-        } else {
-            higher = traverseDelta(q, heightQ - heightP);
-            lower = p;
+            if(b == null) {
+                // start with the other list
+                b = p;
+            } else {
+                b = b.parent;
+            }
         }
 
-        while (higher != lower && higher != null && lower != null) {
-            higher = higher.parent;
-            lower = lower.parent;
-        }
-        return higher;
-
+        return a;
     }
+
+    // private int getHeight(Node node) {
+    //     int count = 0;
+    //     while (node != null) {
+    //         node = node.parent;
+    //         count++;
+    //     }
+    //     return count;
+    // }
+
+    // public Node lowestCommonAncestor(Node p, Node q) {
+
+
+    //     // first count the nodes
+    //     int heightP = getHeight(p);
+    //     int heightQ = getHeight(q);
+
+    //     // System.out.println("Height p : " + heightP);
+
+    //     // System.out.println("Height q : " + heightQ);
+
+    //     Node higher = null;
+    //     Node lower = null;
+        
+    //     while (heightP > heightQ) {
+    //         p = p.parent;
+    //     }
+        
+    //     while (heightQ > heightP) {
+    //         q = q.parent;
+    //     }
+        
+    //     while (p != q && p != null && q != null) {
+    //         p = p.parent;
+    //         q = q.parent;
+    //     }
+        
+    //     return p;
+
+    //     // if (heightP > heightQ) {
+    //     //     higher = traverseDelta(p, heightP - heightQ);
+    //     //     lower = q;
+    //     // } else {
+    //     //     higher = traverseDelta(q, heightQ - heightP);
+    //     //     lower = p;
+    //     // }
+
+    //     // while (higher != lower && higher != null && lower != null) {
+    //     //     higher = higher.parent;
+    //     //     lower = lower.parent;
+    //     // }
+    //     // return higher;
+
+    // }
 }
