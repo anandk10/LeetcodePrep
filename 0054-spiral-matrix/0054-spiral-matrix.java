905a67d29007f913550b0191a3c265e0b47e0b70
Anand Karandikar <anandk10@gmail.com>
2023-12-24 21:12:55 -0800

    Time: 0 ms (100%), Space: 41.7 MB (7.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,4 +1,26 @@
 class Solution {
+
+    /**
+    Idea here is to move horizontally (update j) then vertically (update i)
+    for a single iteration this will look like
+    #####
+        #
+        #
+        # 
+    
+    Horizontal traversal - processes a ROW therefore decrement ROW count
+    Vertical traversal - processes a COL therefore decrement COL count
+    (NO NEED TO BOUNDRY CHECK!!)
+    
+    then flip the direction to negative, by multiplying it by -1
+    Repeat the same traversal, horizontal (update j) then vertical (update i)
+    for the next iteration this will look like
+    
+    #
+    #
+    #####  
+
+     */
     public List<Integer> spiralOrder(int[][] matrix) {
         
         int m = matrix.length;
