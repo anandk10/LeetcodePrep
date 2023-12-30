4786c594cb7c631ff80e6a5a034bc369938e8184
Anand Karandikar <anandk10@gmail.com>
2023-12-29 22:01:27 -0800

    Time: 1 ms (100%), Space: 42.5 MB (67.92%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,23 +1,41 @@
 class Solution {
 
-    private void countChars(String word, int[] freq) {
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            freq[ch - 'a']++;
-        }
-    }
-
     public boolean makeEqual(String[] words) {
+        // only one word then return true
+        if (words.length == 1)
+            return true;
+        
+        // get the total char counts 
+        // and mod by the words length
+        // to check if the characters as 
+        // a whole could be distributed
+        int totalCharCount = 0;
+        for (String word : words) {
+            totalCharCount += word.length();
+        }
+
+        if (totalCharCount % words.length != 0)
+            return false;
+        
+        // to equally distribute the words
+        // we just need to ensure that the 
+        // character frequency for all the  
+        // words can be evenly distributed
         
         int[] freq = new int[26];
-        int len = words.length;
 
-        for (int i = 0; i < len ; i++) {
-            countChars(words[i], freq);
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                freq[ch - 'a']++;
+            }
         }
 
+        // once you've the frequency counts
+        // compare for all the character
+        // frequencies if it can be divided
+        // across the words.length
         for (int fre : freq) {
-            if (fre % len != 0) {
+            if (fre % words.length != 0) {
                 return false;
             }
         }
