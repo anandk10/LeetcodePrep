62b65c79e2669ac93910b244a6d1e326cdb40fce
Anand Karandikar <anandk10@gmail.com>
2023-12-31 22:41:10 -0800

    Time: 1 ms (72.66%), Space: 42.4 MB (19.91%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    List<List<Integer>> result;
+
+    private void dfs(int curr, int[] nums, List<Integer> solution) {
+
+        // base case
+        if (curr == nums.length) {
+            result.add(new ArrayList<>(solution));
+            return;
+        }
+
+        // either add this number to the list, 
+        // because first choice is to include this number
+        solution.add(nums[curr]);
+
+        // with this number added, call dfs to consider rest of the elements
+        dfs(curr + 1, nums, solution);
+
+        // or don't add this number to the list, 
+        // because the second choice to exclude this number
+        // solution.remove(solution.size() - 1);
+        solution.removeLast();
+        
+        // with this number excluded, call dfs to consider rest of the elements
+        dfs(curr + 1, nums, solution);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>();
+
+        // dfs(0, nums, new ArrayList<>());
+        dfs(0, nums, new LinkedList<>());
+        return result;
+        
+    }
+}
