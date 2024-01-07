3b9f6a58ecdb2185a580b26b03e44ec3c4162ea4
Anand Karandikar <anandk10@gmail.com>
2024-01-07 02:22:53 -0800

    Time: 3 ms (38.63%), Space: 45.8 MB (51.54%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -2,49 +2,7 @@ class Solution {
 
     /**
     Perform DFS from node 0 i.e. graph[0] 
-    add 0 to the path, add to the visited set
-    then load neighbors of graph[0]
-    iterate those 
-        while iterating if the node ith is N-1 then 
-        we found a path from 0 to N, add this path to solution
-
-    [[1,2],
-    [3],
-    [3],
-    []]
-
-    [t,f,t,f]
-    
-    0
-    [1,2]
-    
-    1 {0}
-    [3]
-
-    3 {0->1}
-    3==N-1
-
-    {0->1->3} [{0,1,3}]
-
-    1 {0}
-    [...] // 3 is processed
-
-    no neighbors to process
-
-    0 {}
-    [2]
-
-    2 {0}
-    [3]
-
-    3 {0->2}
-    3 == N-1
-
-    {0->2->3} [{0,1,3},{0,2,3}]
-
-    2 {0}
-    [...] // 3 is processed
-
+    once you reach N-1, add the path to the result
      */
 
     boolean[] visited;
@@ -53,27 +11,21 @@ class Solution {
     private void dfs(int currNode, 
                     int[][] graph, 
                     List<Integer> solution) {
-
-        if (currNode == graph.length) return;
-
+        
+        solution.add(currNode);
         if (currNode == graph.length - 1) {
-            solution.addLast(currNode);
             result.add(new ArrayList<>(solution));
-            solution.removeLast();
             return;
         }
 
-        solution.addLast(currNode);
-
         // explore neighbors
         for(int neighbor : graph[currNode]) {
-            visited[neighbor] = true;
             dfs(neighbor, graph, solution);
-            visited[neighbor] = false;
+            if (!solution.isEmpty()) {
+                // remove the node that was added in dfs()
+                solution.removeLast();
+            }
         }
-
-        solution.removeLast();
-
     }
 
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
