d028e9970625acb257cc0e8fea48d2dd000b130d
Anand Karandikar <anandk10@gmail.com>
2024-01-07 12:17:18 -0800

    Time: 293 ms (5.05%), Space: 56.4 MB (91.35%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,5 +1,29 @@
 import java.util.AbstractMap.SimpleEntry;
 
+/**
+
+Three things to leverage:
+1) SORTED ARRAYS - the answer can be derived from either ends 
+(if the same problem asks MAX SUM pair but gives INCREASING ordered array - then start from the right of the arrays)
+2) HEAP - use it to get the next best MAX / MIN sum
+    - store the state in the heap like, the sum, the indices that made it possible
+3) SET - use it to prevent the indices from consideration if they have already been a part of the heap
+    JAVA: Composite Key involving a key -> value construct, is a pain when it comes to comparison. If you intend to use your own class then its a hassle to implement and usually a higher chance to get it wrong. 
+    Unless you depend on JavaFx which provides a Pair class, you've another option by utilizing Map.Entry<K, V> and its concrete implmentation AbstractMap.SimpleEntry<K, V>. Applies for Java 6 and above. 
+
+    import java.util.AbstractMap.SimpleEntry;
+    Set<Map.Entry<Integer, Integer>> visited = new HashSet<>(); 
+    visited.add(new SimpleEntry<>(1,2));
+    visited.contains(new SimpleEntry<>(1,2)); // returns true
+
+    In Java 9+, leverage Map.entry(k, v) to achieve the same effect
+
+    Set<Map.Entry<Integer, Integer>> visited = new HashSet<>(); 
+    visited.add(Map.entry(2,3));
+    visited.contains(Map.entry(2,3)); // returns true
+
+ */
+
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
 
