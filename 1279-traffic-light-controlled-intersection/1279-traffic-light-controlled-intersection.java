5e243c390d9cb0a6fb1288a6fe0ff703b4d559e0
Anand Karandikar <anandk10@gmail.com>
2024-01-13 12:14:55 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.java
+++ 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.java
@@ -1,13 +1,34 @@
 class TrafficLight {
 
     int currentRoad;
-    Semaphore semaphore;
+    // Semaphore semaphore;
+    ReentrantLock lock;
 
     public TrafficLight() {
         this.currentRoad = 1; 
-        this.semaphore = new Semaphore(1);   
+        // this.semaphore = new Semaphore(1);
+        lock = new ReentrantLock();   
     }
     
+    /** Using ReentrantLock */
+    public void carArrived(
+        int carId,           // ID of the car
+        int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,       // Direction of the car
+        Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+        Runnable crossCar    // Use crossCar.run() to make car cross the intersection 
+    ) {
+        this.lock.lock();
+        if (this.currentRoad != roadId) {
+            turnGreen.run(); // turning green for the roadId
+            this.currentRoad = roadId;
+        }
+        crossCar.run();
+        this.lock.unlock();
+    }
+
+    /** Using Semaphore */
+    /**
     public void carArrived(
         int carId,           // ID of the car
         int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
@@ -25,4 +46,5 @@ class TrafficLight {
         this.semaphore.release();
 
     }
+    */
 }
