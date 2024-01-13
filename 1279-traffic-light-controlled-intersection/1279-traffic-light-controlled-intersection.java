672f5322732704c251d3ce1c928eeb5a2ed90465
Anand Karandikar <anandk10@gmail.com>
2024-01-13 12:06:47 -0800

    Time: 13 ms (62.77%), Space: 43.7 MB (10.64%) - LeetHub

---
+++ 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.java
@@ -0,0 +1,28 @@
+class TrafficLight {
+
+    int currentRoad;
+    Semaphore semaphore;
+
+    public TrafficLight() {
+        this.currentRoad = 1; 
+        this.semaphore = new Semaphore(1);   
+    }
+    
+    public void carArrived(
+        int carId,           // ID of the car
+        int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,       // Direction of the car
+        Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+        Runnable crossCar    // Use crossCar.run() to make car cross the intersection 
+    ) {
+        
+        this.semaphore.acquireUninterruptibly();
+        if (this.currentRoad != roadId) {
+            turnGreen.run(); // turning green for the roadId
+            this.currentRoad = roadId;
+        }
+        crossCar.run();
+        this.semaphore.release();
+
+    }
+}
