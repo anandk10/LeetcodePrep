151bdfeee6590bd8c746e7b7e5f173e65e7c51f8
Anand Karandikar <anandk10@gmail.com>
2024-01-21 23:23:42 -0800

    Time: 22 ms (97.39%), Space: 43.2 MB (67.74%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,30 +1,37 @@
 class FooBar {
     private int n;
-    Semaphore foo = new Semaphore(1);
-    Semaphore bar = new Semaphore(0);
-    
+    boolean doBar = false;
+    // Semaphore foo = new Semaphore(1); // the first thread to run uses this
+    // Semaphore bar = new Semaphore(0); // the second thread to run uses this
+
+
     public FooBar(int n) {
         this.n = n;
     }
 
-    public void foo(Runnable printFoo) throws InterruptedException {
-        
+    public synchronized void foo(Runnable printFoo) throws InterruptedException {
+
         for (int i = 0; i < n; i++) {
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-            foo.acquire();
-        	printFoo.run();
-            bar.release();
+            while (doBar) { wait(); }
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            // foo.acquire();
+            printFoo.run();
+            doBar = true;
+            // bar.release();
+            notifyAll();
         }
     }
 
-    public void bar(Runnable printBar) throws InterruptedException {
-        
+    public synchronized void bar(Runnable printBar) throws InterruptedException {
+
         for (int i = 0; i < n; i++) {
-            bar.acquire();
+            while (!doBar) { wait(); }
+            // bar.acquire();
             // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
-            foo.release();
+            printBar.run();
+            // foo.release();
+            doBar = false;
+            notifyAll();
         }
     }
 }
