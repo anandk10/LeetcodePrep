12f17ee2b4960d8ce0766a94ed4b77b95002ee91
Anand Karandikar <anandk10@gmail.com>
2024-02-13 00:23:24 -0800

    Time: 0 ms (100%), Space: 41.5 MB (58.76%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,31 +1,35 @@
 class Solution {
+
+    /**
+    - Compare the lengths of s1 and s2, if unequal, return false.
+    - Track the count of differing characters and their indices.
+    - Iterate through each character index.
+    - If characters at the same index are different:
+    - If the count exceeds 2, return false.
+    - Record the index of the differing characters.
+    - Check if either there are no differences or exactly two differences with swapped characters.
+    - Return true if conditions in step 5 are met, else return false.
+     */
     public boolean areAlmostEqual(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;  
         }
 
-        // bank - 
-        //    i
-        // kanb
-        // 
-        // solution - 
-        //        i
-        // solntiou
-        int[] indices = new int[2];
-
         int count = 0;
+        int index1 = -1; 
+        int index2 = -1;
+        
         for (int i = 0 ; i < s1.length(); i++) {
-
             if (s1.charAt(i) != s2.charAt(i)) {
                 if (count == 2) return false;
-                indices[count] = i;
-                count += 1;
+                if (count == 0) index1 = i;
+                else index2 = i;
+                count++;
             }
-
         }
 
-
-        return count == 0 || (s1.charAt(indices[0]) == s2.charAt(indices[1]) &&
-            s1.charAt(indices[1]) == s2.charAt(indices[0]));
+        return count == 0 || (count == 2 && 
+                              s1.charAt(index1) == s2.charAt(index2) &&
+                              s1.charAt(index2) == s2.charAt(index1));
     }
 }
