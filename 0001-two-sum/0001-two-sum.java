df4b0d7bf442589b4c8719c22370b61ebc493761
Anand Karandikar <anandk10@gmail.com>
2025-12-28 19:55:55 -0800

    Time: 5 ms (50.08%), Space: 46.7 MB (56.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,35 +1,34 @@
 class Solution {
-    
-    private int[] solveTwoSum(int[] nums, int target) {
-        // traverse the nums and if you're seeing that num
-        // for the first time then add it to a Map
-        // for every number compare if the target - num is
-        // hashed in the map. this indicates that the previous
-        // num that forms a pair is seen.
-        
-        if (nums.length < 2) {
-            return new int[]{};
-        }
-        
-        int[] res = new int[2];
-        
-        Map<Integer, Integer> pairSum = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            // found a pair
-            if (pairSum.containsKey(target - nums[i])) {
-                return new int[]{pairSum.get(target - nums[i]), i};
-            }
-            
-            // haven't found a pair yet
-            pairSum.put(nums[i], i); 
-        }
-        
-        return res;
-        
-    }
-    
     public int[] twoSum(int[] nums, int target) {
-        return solveTwoSum(nums, target);    
+        // 2 --> 0
+        // 7 --> 1
+        // 11 --> 2
+        // 15 --> 3
+
+        return solution(nums, target);
+
+    }
+
+    public int[] solution(int[] nums, int target) {
+        int[] answer = {0,0};
+
+        // create a hashmap and store the <num, index> in it
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // first case: 9 - 2 (nums[i] ; i = 0) = 7 ; now checking for 7 in map
+            Integer secondIndex = numMap.get(target - nums[i]);
+            if (secondIndex != null && secondIndex != i) {
+                answer[0] = i;
+                answer[1] = secondIndex;
+                break;
+            }
+        }
+
+
+        return answer;
     }
 }
